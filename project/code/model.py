54ce7a6b41cd43c3a712a305ce91a7ac24eb3558
Abby Goldberg <abby_goldberg@brown.edu>
2020-11-30 18:04:15 -0500

    worked on the model's call function

--- project/code/model.py
+++ project/code/model.py
@@ -2,11 +2,11 @@ import numpy as np
 import tensorflow as tf
 from attention import Attention
 
-class GRU_Model(tf.keras.Model):
+class Model(tf.keras.Model):
     def __init__(self, vocab_size):
-        ###### DO NOT CHANGE ##############
-        super(GRU_Model, self).__init__()
+        super(Model, self).__init__()
 
+        self.vocab_size = vocab_size
 
         # Define hyperparameters
         self.encoder_decoder_size = 40
@@ -28,31 +28,37 @@ class GRU_Model(tf.keras.Model):
         self.decoder = tf.keras.layers.GRU(
             self.encoder_decoder_size, return_sequences=True, return_state=True)
         self.attention_layer = Attention(self.encoder_decoder_size)
+        self.dense_layer1 = tf.keras.layers.Dense(
+            self.encoder_decoder_size, activation='tanh', dtype=tf.float32)
+        self.dense_layer2 = tf.keras.layers.Dense(
+            self.vocab_size, activation=None, dtype=tf.float32)
 
     @tf.function
     def call(self, encoder_input, decoder_input):
         """
         :param encoder_input: batched ids corresponding to input sentences
         :param decoder_input: batched ids corresponding to reversed output sentences
-        :return prbs: The 3d probabilities as a tensor, [batch_size x window_size x english_vocab_size]
+        :return prbs: The 3d probabilities as a tensor, [batch_size x window_size x vocab_size]
         """
-
-        # NOT DONE
-
         # Pass encoder sentence embeddings to the encoder
         encoder_embeddings = tf.nn.embedding_lookup(
             self.embeddings, encoder_input)
         encoder_output, encoder_final_state = self.encoder(
             encoder_embeddings, initial_state=None)
 
-        # Pass decoder embeddings and final state of the encoder to decoder
+        # Use general attention
+        context = self.attention(encoder_final_state, encoder_output)
+        context_hidden_concat = tf.concat([context, encoder_final_state], axis=-1) # axis?
+        hidden_with_attention = self.dense_layer1(context_hidden_concat)
+
+        # Pass decoder embeddings and attention enhanced hidden state to decoder
         decoder_embeddings = tf.nn.embedding_lookup(
             self.embeddings, decoder_input)
         decoder_output, decoder_final_state = self.decoder(
             decoder_embeddings,
-            initial_state=encoder_final_state)
+            initial_state=hidden_with_attention)
 
         # Apply dense layer to the decoder out to generate probabilities
-        dense_outputs = self.dense(decoder_output)
+        dense_outputs = self.dense_layer2(decoder_output)
 
         return tf.nn.softmax(dense_outputs)
